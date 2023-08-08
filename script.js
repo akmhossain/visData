910384a6796a255d9a4bd26475130d7e6386e360
akmhossain <akmhossain101@gmail.com>
2023-08-08 15:47:39 -0400

    final

---
+++ script.js
@@ -0,0 +1,34 @@
+let num = Math.floor(Math.random()*10)
+let change_button = document.getElementById("new")
+let name_ = document.getElementById("name")
+let img = document.querySelector("img")
+
+    let city_button = document.getElementById("city")
+    function city() {
+    city_button.innerHTML = `${data.results[num].location.city}`
+    }
+    city_button.addEventListener('click', city)
+
+    let email_button = document.getElementById("email")
+    function email() {
+    email_button.innerHTML = `${data.results[num].email}`
+    }
+    email_button.addEventListener('click', email)
+
+    let username_button = document.getElementById("username")
+    function username() {
+    username_button.innerHTML = `${data.results[num].login.username}`
+    }
+    username_button.addEventListener('click', username)
+
+    let age_button = document.getElementById("age")
+    function age() {
+    age_button.innerHTML = `${data.results[num].dob.age}`
+    }
+    age_button.addEventListener('click', age) 
+
+function change() {
+        name_.innerHTML = `${data.results[num].name.first} ${data.results[num].name.last}`
+        img.src = `${data.results[num].picture.large}`; 
+    }
+change_button.addEventListener('click', change)
